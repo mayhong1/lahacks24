cae5d7f8d7555dec10c54a4c02aa9332fe40532e
eghazarian1223 <ghaza034@umn.edu>
2024-04-20 01:26:45 -0700

    Add navigation bar component

---
+++ frontend/components.py
@@ -0,0 +1,3 @@
+# for reusable UI components like buttons
+from reflex import Component
+from reflex import html
---
+++ frontend/views.py
@@ -0,0 +1,4 @@
+#this will work with components.py to allow users to interact w/ the webapp
+from reflex import Component
+from reflex import html
+from .components import Button, TextInput, PlaylistCard 
